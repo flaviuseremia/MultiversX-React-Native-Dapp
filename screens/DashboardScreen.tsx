fc7a7acfd5087c0460a4ddbd5753bb6413e89c51
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-12 18:04:46 +0200

    Create DashboardScreen

---
+++ screens/DashboardScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function DashboardScreen() {
+  return (
+    <View>
+      <Text>Dashboard!</Text>
+    </View>
+  );
+}
+
+export default DashboardScreen;
