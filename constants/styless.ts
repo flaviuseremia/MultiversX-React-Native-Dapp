15948c571d1df898a124de37701b702f127c6fc9
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-12 18:04:33 +0200

    Create pallet of colors

---
+++ constants/styless.ts
@@ -0,0 +1,18 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#e4d9fd",
+    primary100: "#c6affc",
+    primary200: "#a281f0",
+    primary400: "#5721d4",
+    primary500: "#3e04c3",
+    primary700: "#0C2D59",
+    primary800: "#130823",
+    accent500: "#f7bc0c",
+    secondary50: "#fcc4e4",
+    secondary200: "#C147FF",
+    error500: "#9b095c",
+    gray500: "#39324a",
+    gray600: "#26222e",
+    gray700: "#221c30",
+  },
+};
