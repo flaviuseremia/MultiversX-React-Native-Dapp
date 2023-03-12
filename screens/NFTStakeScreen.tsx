40cb0d07bc95c136f8f5c2c9232f1e1dadd77b3c
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-12 18:04:11 +0200

    Create NFTStakeScreen

---
+++ screens/NFTStakeScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+function NFTStakeScreen() {
+return(
+    <View>
+        <Text>NFT Stake!</Text>
+    </View>
+);
+}
+
+export default NFTStakeScreen;
