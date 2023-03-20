5779f5f1acbff102ab5add8e30720b40a3779f1c
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-20 17:12:47 +0200

    Update UnstakeScreen

--- screens/UnstakeScreen.tsx
+++ screens/UnstakeScreen.tsx
@@ -1,11 +1,144 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import Settings from "../components/Settings";
+import TotalEgld from "../components/TotalEgld";
+import { SelectList } from "react-native-dropdown-select-list";
+import { useState } from "react";
+import { GlobalStyles } from "../constants/styless";
+import Button from "../components/UI/Button";
 
 function UnstakeScreen() {
-return(
-    <View>
-        <Text>Unstake!</Text>
-    </View>
-);
+  const [selectedProvider, setSelectedProvider] = useState("");
+
+  const data = [
+    { key: "1", value: "Trust Staking" },
+    { key: "2", value: "Titan Stake" },
+    { key: "3", value: "Carpathian Stake" },
+  ];
+
+  function emptyHandle() {}
+  const [number, setNumber] = useState("");
+
+  const handleInputChange = (value: string) => {
+    const regex = /^[0-9]*$/;
+    if (regex.test(value)) {
+      setNumber(value);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Settings name="flavius11" />
+      <View style={styles.textContainer}>
+        <TotalEgld title="Total EGLD Staked" sum="100.00" />
+      </View>
+      <SelectList
+        data={data}
+        setSelected={(val: string) => setSelectedProvider(val)}
+        save="value"
+        inputStyles={{ color: GlobalStyles.colors.secondary200 }}
+        dropdownTextStyles={{ color: GlobalStyles.colors.secondary200 }}
+        boxStyles={{ backgroundColor: GlobalStyles.colors.primary700 }}
+        dropdownStyles={{ backgroundColor: GlobalStyles.colors.primary700 }}
+        placeholder="Select Provider"
+      />
+      <View style={styles.displayContainer}>
+        <Text style={styles.displayText}>
+          <Text style={styles.xText}>X</Text> EGLD staked
+        </Text>
+      </View>
+      <KeyboardAvoidingView style={styles.keyboardContainer} behavior="height">
+        <View style={styles.inputAndTextContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              value={number}
+              onChangeText={handleInputChange}
+              keyboardType="numeric"
+              returnKeyType="done"
+              blurOnSubmit={true}
+            />
+          </View>
+          <Text style={styles.text}>EGLD</Text>
+        </View>
+      </KeyboardAvoidingView>
+      <View style={styles.buttonContainer}>
+        <Button
+          children={"Unstake"}
+          onPress={emptyHandle}
+          style={styles.button}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  textContainer: {
+    alignItems: "center",
+  },
+  displayContainer: {
+    backgroundColor: GlobalStyles.colors.primary700,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 5,
+    height: "20%",
+    borderRadius: 8,
+  },
+  displayText: {
+    fontSize: 28,
+    color: "white",
+  },
+  xText: {
+    color: GlobalStyles.colors.secondary200,
+  },
+  keyboardContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputAndTextContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputContainer: {
+    borderWidth: 1,
+    backgroundColor: GlobalStyles.colors.primary700,
+    borderRadius: 8,
+    padding: 8,
+    minWidth: "20%",
+  },
+  input: {
+    fontSize: 18,
+    color: "white",
+  },
+  text: {
+    color: GlobalStyles.colors.secondary200,
+    fontSize: 20,
+    marginLeft: 10,
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 40,
+    minWidth: "30%",
+  },
+});
 
 export default UnstakeScreen;
