26c14251ac01b69fe3a3dd9d42f5675cf95a889a
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-25 14:41:27 +0200

    Add Stake Balance to context

--- store/UserContext.tsx
+++ store/UserContext.tsx
@@ -1,10 +1,12 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, Dispatch, SetStateAction } from "react";
 
 interface UserContextType {
   username: string;
   availableBalance: string;
+  stakedBalance: string;
   setUsername: (username: string) => void;
-  setAvailableBalance: (availableBalance: string) => void;
+  setAvailableBalance: Dispatch<SetStateAction<string>>;
+  setStakedBalance: Dispatch<SetStateAction<string>>;
 }
 
 type UserProviderProps = {
@@ -14,19 +16,24 @@ type UserProviderProps = {
 export const UserContext = createContext<UserContextType>({
   username: "",
   availableBalance: "",
+  stakedBalance: "",
   setUsername: () => {},
   setAvailableBalance: () => {},
+  setStakedBalance: () => {},
 });
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [username, setUsername] = useState("");
   const [availableBalance, setAvailableBalance] = useState("");
+  const [stakedBalance, setStakedBalance] = useState("");
 
   const value = {
     username: username,
     availableBalance: availableBalance,
+    stakedBalance: stakedBalance,
     setUsername: setUsername,
     setAvailableBalance: setAvailableBalance,
+    setStakedBalance: setStakedBalance,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
