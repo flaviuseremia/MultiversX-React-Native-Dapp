7e61f06bbf7c5da33d9b74e0c786d9bfdd96e44b
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-25 14:56:59 +0200

    Add rewards Balance to context

--- store/UserContext.tsx
+++ store/UserContext.tsx
@@ -1,12 +1,19 @@
-import React, { createContext, useState, Dispatch, SetStateAction } from "react";
+import React, {
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from "react";
 
 interface UserContextType {
   username: string;
   availableBalance: string;
   stakedBalance: string;
+  rewardsBalance: string;
   setUsername: (username: string) => void;
   setAvailableBalance: Dispatch<SetStateAction<string>>;
   setStakedBalance: Dispatch<SetStateAction<string>>;
+  setRewardsBalance: Dispatch<SetStateAction<string>>;
 }
 
 type UserProviderProps = {
@@ -17,23 +24,28 @@ export const UserContext = createContext<UserContextType>({
   username: "",
   availableBalance: "",
   stakedBalance: "",
+  rewardsBalance: "",
   setUsername: () => {},
   setAvailableBalance: () => {},
   setStakedBalance: () => {},
+  setRewardsBalance: () => {},
 });
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [username, setUsername] = useState("");
   const [availableBalance, setAvailableBalance] = useState("");
   const [stakedBalance, setStakedBalance] = useState("");
+  const [rewardsBalance, setRewardsBalance] = useState("");
 
   const value = {
     username: username,
     availableBalance: availableBalance,
     stakedBalance: stakedBalance,
+    rewardsBalance: rewardsBalance,
     setUsername: setUsername,
     setAvailableBalance: setAvailableBalance,
     setStakedBalance: setStakedBalance,
+    setRewardsBalance: setRewardsBalance,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
