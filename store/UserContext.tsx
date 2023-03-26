8d5918cc8317e9bc901ee6efb344b0e9e4fe8736
flaviuseremia <flaviuseremia11@gmail.com>
2023-03-26 16:39:01 +0300

    Add providers to context

--- store/UserContext.tsx
+++ store/UserContext.tsx
@@ -11,11 +11,13 @@ interface UserContextType {
   availableBalance: string;
   stakedBalance: string;
   rewardsBalance: string;
+  providers: { key: string; value: string };
   setUsername: (username: string) => void;
   setTotalBalance: Dispatch<SetStateAction<string>>;
   setAvailableBalance: Dispatch<SetStateAction<string>>;
   setStakedBalance: Dispatch<SetStateAction<string>>;
   setRewardsBalance: Dispatch<SetStateAction<string>>;
+  setProviders: (providers: { key: string; value: string }) => void;
 }
 
 type UserProviderProps = {
@@ -28,11 +30,13 @@ export const UserContext = createContext<UserContextType>({
   availableBalance: "",
   stakedBalance: "",
   rewardsBalance: "",
+  providers: { key: "", value: "" },
   setUsername: () => {},
   setTotalBalance: () => {},
   setAvailableBalance: () => {},
   setStakedBalance: () => {},
   setRewardsBalance: () => {},
+  setProviders: () => {},
 });
 
 export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
@@ -41,6 +45,10 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
   const [availableBalance, setAvailableBalance] = useState("");
   const [stakedBalance, setStakedBalance] = useState("");
   const [rewardsBalance, setRewardsBalance] = useState("");
+  const [providers, setProviders] = useState<{ key: string; value: string }>({
+    key: "",
+    value: "",
+  });
 
   const value = {
     username: username,
@@ -48,11 +56,13 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     availableBalance: availableBalance,
     stakedBalance: stakedBalance,
     rewardsBalance: rewardsBalance,
+    providers: providers,
     setUsername: setUsername,
     setTotalBalance: setTotalBalance,
     setAvailableBalance: setAvailableBalance,
     setStakedBalance: setStakedBalance,
     setRewardsBalance: setRewardsBalance,
+    setProviders: setProviders,
   };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
